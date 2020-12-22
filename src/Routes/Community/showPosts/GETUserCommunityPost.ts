0cee06675b43961281229543effb88d5fb9414b4
postmelee <meleeisdeveloping@gmail.com>
2020-12-22 20:19:52 +0900

    Profile Post Swiper & Bottom Sheet(not cleaned)

---
+++ src/Routes/Community/showPosts/GETUserCommunityPost.ts
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import serverConfig from '../../server.config';
+
+const baseUri = serverConfig.baseUri;
+
+interface Props {
+    type: string;
+    limit: number;
+    offset: number;
+    order: string;
+}
+
+const GETUserCommunityPosts = (jwtToken: string, nickname: string, {type, limit, offset, order}: Props) => {
+    const uri = baseUri + `/api/v1/users/${nickname}/communities?limit=${limit}&offset=${offset}&order=${order}&type=${type}`;
+    console.log(type, limit, offset, order);
+    return new Promise(function(resolve, reject) {
+
+        axios.get(uri, {
+            headers: {
+              'Authorization': jwtToken,
+            },
+        })
+        .then(function(response) {
+            console.log('GETUserCommunityPosts SUCCESS',response)
+            resolve(response.data);
+            
+        })
+        .catch(function(error) {
+            console.log('fail',error)
+            reject(error.response);
+        })
+
+    })
+}
+
+export default GETUserCommunityPosts;
+
